bbdd41a3d2f00838e6dbe9935167efb3bb243118
JoseGabino12 <gabinoMorales1212@hotmail.com>
2023-06-06 19:33:58 -0600

    Feature: board created co-authored-by: Juan Michel juanmmv16@gmail.com

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,14 +1,73 @@
 class GameLife
-    def mostrar_pista(row, columns)
+    def start_game(row, columns)
         tablero = []
         for i in(0...row)
             tablero.insert(i, []);
             for j in(0...columns)
-                tablero[i].insert(j, "*")
+                if rand(0..10) == 1
+                    tablero[i].insert(j, "*")
+                else
+                    tablero[i].insert(j, ".")
+                end
             end
-            puts "#{tablero[i]}"
+            puts "#{tablero[i].join(" ")}"
         end
+
+        return tablero
+    end
+
+    def next_generation(tablero)
+        indice_celula_viva = []
+
+        tablero.each_with_index do |row, i|
+            row.each_with_index do |column, j|
+                if tablero[i][j] == "*"
+                    indice_celula_viva.push([i,j])
+                end
+            end
+        end
+
+        indice_celula_viva.each do |i, j|
+            colum = j
+            if i < tablero.length - 1
+            puts "i: #{i} j: #{j}"
+                if tablero[i][colum + 1 ] == ("." || nil)
+                    puts "#{tablero[i][j+1]}"
+                    if tablero[i][colum - 1] == ("." || nil)
+                        puts "#{tablero[i][j-1]}"
+                        if tablero[i+1][j] == ("." || nil)
+                            puts "#{tablero[i+1][j]}"
+                            if tablero[i-1][j] == ("." || nil)
+                                puts "#{tablero[i-1][j]}"
+                                if tablero[i+1][j+1] == ("." || nil)
+                                    puts "#{tablero[i+1][j+1]}"
+                                    if tablero[i-1][j-1] == ("." || nil)
+                                        puts "#{tablero[i-1][j-1]}"
+                                        if tablero[i+1][j-1] == ("." || nil)
+                                            puts "#{tablero[i+1][j-1]}"
+                                            if tablero[i-1][j+1] == ("." || nil)
+                                                puts "#{tablero[i-1][j+1]}"
+                                                tablero[i][j] = "."
+                                            end
+                                        end
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+
+
+        tablero.each_with_index do |row, i|
+            puts "#{tablero[i].join(" ")}"
+        end
+
     end
 end
 
-GameLife.new.mostrar_pista(4,8)
+tablero = GameLife.new.start_game(4,8)
+puts "-------------------------"
+puts "Next generation"
+first_generation = GameLife.new.next_generation(tablero)
